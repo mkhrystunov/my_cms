b869e86ef149e2826b1ba3393fdded9c9d7195b1
max <khristunov93@gmail.com>
2015-01-24 12:23:12 +0200

    Initial commit

---
+++ src/Devy/UkrBookBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Devy\UkrBookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('DevyUkrBookBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Devy/UkrBookBundle/DevyUkrBookBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Devy\UkrBookBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DevyUkrBookBundle extends Bundle
+{
+}
