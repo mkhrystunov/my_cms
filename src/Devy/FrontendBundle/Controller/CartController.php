2bdd472d5d6392d2407997f122c84262325d718b
max <mkhristunov@experty.com>
2015-02-12 18:10:36 +0200

    Ajax cart

--- src/Devy/FrontendBundle/Controller/CartController.php
+++ src/Devy/FrontendBundle/Controller/CartController.php
@@ -5,6 +5,7 @@ namespace Devy\FrontendBundle\Controller;
 use Devy\FrontendBundle\DependencyInjection\Cart;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -16,23 +17,31 @@ class CartController extends Controller
     /**
      * @param Request $request
      * @param int $productId
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function addAction(Request $request, $productId)
     {
         /** @var EntityManager $manager */
         $manager = $this->getDoctrine()->getManager();
         $product = $manager->getRepository('DevyUkrBookBundle:Product')->find($productId);
-        $session = $request->getSession();
-        /** @var Cart $cart */
-        $cart = $session->get('cart', new Cart());
 
         if ($product) {
+            $session = $request->getSession();
+            /** @var Cart $cart */
+            $cart = $session->get('cart', new Cart());
             $cart->addProduct($product->getId());
-            $this->get('session')->getFlashBag()->add('success', sprintf('%s was added', $product->getTitle()));
-        } else {
-            $this->get('session')->getFlashBag()->add('notice', sprintf('Some error with product.'));
-        }
+            $session->set('cart', $cart);
 
-        $session->set('cart', $cart);
+            return new JsonResponse([
+                'success' => true,
+                'message' => sprintf('%s was added', $product->getTitle()),
+                'count' => $cart->getCount(),
+            ]);
+        } else {
+            return new JsonResponse([
+                'success' => false,
+                'message' => 'Some error with product. Please try again.',
+            ]);
+        }
     }
 }
